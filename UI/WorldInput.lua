528e7d92837c1e01a92bf6d571aa368fa2f6eb57
chaorace <chaorace@gmail.com>
2016-11-05 14:25:22 -0400

    Shift doesn't toggle cityview in tech tree
    Fixes #38

--- UI/WorldInput.lua
+++ UI/WorldInput.lua
@@ -1017,7 +1017,7 @@ function DefaultKeyUpHandler( uiKey:number )
 					UI.SelectPrevReadyUnit();
 				end
 			end
-			if( uiKey == Keys.VK_SHIFT ) then
+			if( uiKey == Keys.VK_SHIFT and ContextPtr:LookUpControl("/InGame/TechTree"):IsHidden()) then
 				if(CQUI_cityview) then
 					LuaEvents.CQUI_CityviewDisable();
 					UI.SelectNextReadyUnit();
