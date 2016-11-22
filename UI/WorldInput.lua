Azurency/CQUI_Community-Edition

commit 70c5fbbcffa08d36576220abc01813cc7c295d92

Added Civ V Keybinds! Two new modes: Classic & Enhanced
Classic is a very close replication of the Civ V keybind scheme. Enhanced is a modification with WASD camera controls, Q/E unit/city cycling, and Shift for toggling between city/unit selection

UI/WorldInput.lua
@@ -128,7 +128,7 @@ local m_kTutorialUnitMoveRestrictions	:table = nil;		-- Restrictions for moving 
 
 --CQUI MEMBERS
 local CQUI_cityview = false;
-
+local CQUI_hotkeyMode = 1; -- 0: V-Style with enhancements 1: V-Style 2: No changes
 
 -- ===========================================================================
 --	FUNCTIONS
@@ -916,15 +916,141 @@ end
 
 -- ===========================================================================
 -- ===========================================================================
+function CQUI_BuildImprovement (unit, improvementHash: number)
+	local tParameters = {};
+	tParameters[UnitOperationTypes.PARAM_X] = unit:GetX();
+	tParameters[UnitOperationTypes.PARAM_Y] = unit:GetY();
+	tParameters[UnitOperationTypes.PARAM_IMPROVEMENT_TYPE] = improvementHash;
+
+	UnitManager.RequestOperation( unit, UnitOperationTypes.BUILD_IMPROVEMENT, tParameters );
+end
+
 function DefaultKeyDownHandler( uiKey:number )
 	local keyPanChanged :boolean = false;
-    if uiKey == Keys.VK_ALT then
+	
+	if uiKey == Keys.VK_ALT then
 		if m_isALTDown == false then
 			m_isALTDown = true;
 			EndDragMap();
 			ReadyForDragMap();
 		end
     end
+	
+	--CQUI Keybinds
+	if CQUI_hotkeyMode ~= 0 then
+		if CQUI_hotkeyMode == 2 then
+			if( uiKey == Keys.W ) then
+				keyPanChanged = true;
+				m_isUPpressed = true;
+			end
+			if( uiKey == Keys.D ) then
+				keyPanChanged = true;
+				m_isRIGHTpressed = true;
+			end
+			if( uiKey == Keys.S ) then
+				keyPanChanged = true;
+				m_isDOWNpressed = true;
+			end
+			if( uiKey == Keys.A ) then
+				keyPanChanged = true;
+				m_isLEFTpressed = true;
+			end
+			if( uiKey == Keys.E ) then
+				if(CQUI_cityview) then
+					LuaEvents.CQUI_GoNextCity();
+				else
+					UI.SelectNextReadyUnit();
+				end
+			end
+			if( uiKey == Keys.Q ) then
+				if(CQUI_cityview) then
+					LuaEvents.CQUI_GoPrevCity();
+				else
+					UI.SelectPrevReadyUnit();
+				end
+			end
+			
+			if( uiKey == Keys.VK_SHIFT ) then
+				if(CQUI_cityview) then
+					LuaEvents.CQUI_CityviewDisable();
+					UI.SelectNextReadyUnit();
+				else
+					LuaEvents.CQUI_CityviewEnable();
+					LuaEvents.CQUI_GoNextCity();
+				end
+			end
+		else --Classic binds that would overlap with the enhanced binds
+			if( uiKey == Keys.Q ) then
+				CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), -80493497); --Quarry
+			end
+			if( uiKey == Keys.S ) then
+				UnitManager.RequestCommand(UI.GetHeadSelectedUnit(), UnitOperationTypes.AIR_ATTACK);
+			end
+		end
+		-- Fortify until healed hotkey
+		if( uiKey == Keys.H ) then
+			UnitManager.RequestOperation(UI.GetHeadSelectedUnit(), 2126026491); -- OH MY GOD IT WAS SO HARD TO FIND THAT ID. WHAT WAS FIRAXIS THINKING
+		end
+		-- Focus Capital hotkey
+		if( uiKey == Keys.VK_HOME ) then
+			UI.SelectCity(Players[Game.GetLocalPlayer()]:GetCities():GetCapitalCity());
+		end
+		if( uiKey == Keys.VK_BACK ) then
+			UnitManager.RequestCommand(UI.GetHeadSelectedUnit(), UnitCommandTypes.CANCEL);
+		end
+		if( uiKey == Keys.I ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 168372657); --Farm
+		end
+		if( uiKey == Keys.P ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 154488225); --Pasture
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 1523996587); --Plantation
+		end
+		if( uiKey == Keys.N ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 1001859687); --Mine
+			UnitManager.RequestOperation(UI.GetHeadSelectedUnit(), UnitOperationTypes.WMD_STRIKE);
+		end
+		if( uiKey == Keys.H ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), -1819558972); --Camp
+		end
+		if( uiKey == Keys.H ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 2048582848); --Lumber Mill
+		end
+		if( uiKey == Keys.R ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 115772143); --Road
+			UnitManager.RequestOperation(UI.GetHeadSelectedUnit(), UnitOperationTypes.REBASE);
+		end
+		if( uiKey == Keys.F ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 578093457); --Fishing Boats
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), 1694280827); --Fort
+			UnitManager.RequestOperation(UI.GetHeadSelectedUnit(), -41338758); --Sleep
+		end
+		if( uiKey == Keys.O ) then
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), -1355513600); --Oil Well
+			CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), -396628467); --Offshore Platform
+		end
+		if( uiKey == Keys.Y ) then
+			LuaEvents.CQUI_Option_ToggleYields();
+		end
+		if( uiKey == Keys.VK_OEM_PERIOD ) then
+			UI.SelectNextReadyUnit();
+		end
+		if( uiKey == Keys.VK_OEM_COMMA ) then
+			UI.SelectPrevReadyUnit();
+		end
+		if( m_isALTDown ) then --Overridden classic binds get an alt-version as well as the normal alt-shortcuts
+			if( uiKey == Keys.C ) then
+				UnitManager.RequestOperation(UI.GetHeadSelectedUnit(), UnitOperationTypes.HARVEST_RESOURCE);
+				UnitManager.RequestOperation(UI.GetHeadSelectedUnit(), UnitOperationTypes.REMOVE_FEATURE);
+			end
+			if( uiKey == Keys.Q ) then
+				CQUI_BuildImprovement(UI.GetHeadSelectedUnit(), -80493497); --Quarry
+			end
+			if( uiKey == Keys.S ) then
+				UnitManager.RequestCommand(UI.GetHeadSelectedUnit(), UnitOperationTypes.AIR_ATTACK);
+			end
+		end
+	end
+
     if( uiKey == Keys.VK_UP ) then
 		keyPanChanged = true;
 		m_isUPpressed = true;
@@ -950,8 +1076,28 @@ end
 -- ===========================================================================
 -- ===========================================================================
 function DefaultKeyUpHandler( uiKey:number )
-	
 	local keyPanChanged	:boolean = false;
+
+	--CQUI Keybinds
+	if CQUI_hotkeyMode == 2 then
+		if( uiKey == Keys.W ) then
+			m_isUPpressed = false;
+			keyPanChanged = true;
+		end
+		if( uiKey == Keys.D ) then
+			m_isRIGHTpressed = false;
+			keyPanChanged = true;
+		end
+		if( uiKey == Keys.S ) then
+			m_isDOWNpressed = false;
+			keyPanChanged = true;
+		end
+		if( uiKey == Keys.A ) then
+			m_isLEFTpressed = false;
+			keyPanChanged = true;
+		end
+	end
+	
     if uiKey == Keys.VK_ALT then
 		if m_isALTDown == true then
 			m_isALTDown = false;
@@ -1087,6 +1233,7 @@ end
 function OnInterfaceModeEnter_CityManagement( eNewMode:number )
 	UIManager:SetUICursor(CursorTypes.RANGE_ATTACK);	
 	UILens.SetActive("CityManagement");
+	UILens.SetLayerHexesColoredArea( LensLayers.CITY_MANAGEMENT, Game.GetLocalPlayer(), UI.GetHeadSelectedCity():GetCulture():GetNextPlot(), UI.GetColorValue("COLOR_DISGUSTING_APPEAL") );
 end
 
 -- ===========================================================================
@@ -3344,8 +3491,9 @@ function Initialize()
 	ContextPtr:SetShutdown( OnShutdown );
 	
 	-- CQUI Events
-	LuaEvents.CQUI_WorldInput_CityviewEnabled.Add( function() CQUI_cityview = true; end );
-	LuaEvents.CQUI_WorldInput_CityviewDisabled.Add( function() CQUI_cityview = false; end );
+	LuaEvents.CQUI_WorldInput_CityviewEnable.Add( function() CQUI_cityview = true; end );
+	LuaEvents.CQUI_WorldInput_CityviewDisable.Add( function() CQUI_cityview = false; end );
+	LuaEvents.CQUI_Option_ToggleBindings.Add( function(mode: number) CQUI_hotkeyMode = mode; end );
 	
 	Controls.DebugStuff:SetHide(not m_isDebuging);
 	-- Popup setup

UI/MinimapPanel.lua
@@ -49,6 +49,20 @@ local m_wasMouseInMinimap		:boolean = false; -- Was the mouse over the minimap t
 --	FUNCTIONS
 -- ===========================================================================
 
+-- CQUI Options Panel logic
+function CQUI_OnToggleBindings(mode: number)
+	Controls.CQUI_ToggleBindings0:SetCheck(false);
+	Controls.CQUI_ToggleBindings1:SetCheck(false);
+	Controls.CQUI_ToggleBindings2:SetCheck(false);
+	if(mode == 0) then
+		Controls.CQUI_ToggleBindings0:SetCheck(true);
+	elseif(mode == 1) then
+		Controls.CQUI_ToggleBindings1:SetCheck(true);
+	elseif(mode == 2) then
+		Controls.CQUI_ToggleBindings2:SetCheck(true);
+	end
+end
+
 -- ===========================================================================
 function GetContinentsCache()
 	if m_ContinentsCache == nil then
@@ -166,8 +180,10 @@ function ToggleYieldIcons()
 	UserConfiguration.ShowMapYield( showMapYield );
 	if showMapYield then
 		LuaEvents.MinimapPanel_ShowYieldIcons();
+		Controls.ToggleYieldsButton:SetCheck(true);
 	else
 		LuaEvents.MinimapPanel_HideYieldIcons();
+		Controls.ToggleYieldsButton:SetCheck(false);
 	end
 end
 
@@ -749,6 +765,9 @@ function Initialize()
 	
 	Controls.CQUI_ToggleTechPopup:RegisterCallback( Mouse.eLClick, function() LuaEvents.CQUI_Option_ToggleTechPopup(); end);
 	Controls.CQUI_ToggleTechVoiceover:RegisterCallback( Mouse.eLClick, function() LuaEvents.CQUI_Option_ToggleTechVoiceover(); end);
+	Controls.CQUI_ToggleBindings2:RegisterCallback( Mouse.eLClick, function() LuaEvents.CQUI_Option_ToggleBindings(2); end);
+	Controls.CQUI_ToggleBindings1:RegisterCallback( Mouse.eLClick, function() LuaEvents.CQUI_Option_ToggleBindings(1); end);
+	Controls.CQUI_ToggleBindings0:RegisterCallback( Mouse.eLClick, function() LuaEvents.CQUI_Option_ToggleBindings(0); end);
 
     -- Make sure the StrategicSwitcherButton has the correct image when the game starts in StrategicView
     if UI.GetWorldRenderView() == WorldRenderView.VIEW_2D then
@@ -764,5 +783,9 @@ function Initialize()
 	LuaEvents.NotificationPanel_ShowContinentLens.Add(OnToggleContinentLensExternal);
 	LuaEvents.Tutorial_DisableMapDrag.Add( OnTutorial_DisableMapDrag );
     LuaEvents.Tutorial_SwitchToWorldView.Add( OnTutorial_SwitchToWorldView );
+	
+	-- CQUI Handlers
+	LuaEvents.CQUI_Option_ToggleBindings.Add( CQUI_OnToggleBindings );
+	LuaEvents.CQUI_Option_ToggleYields.Add( ToggleYieldIcons );
 end
 Initialize();

UI/MinimapPanel.xml
@@ -31,18 +31,35 @@
 		<Image Texture="Controls_ButtonExtendSmall" TextureOffset="0,60" Size="20,20" Anchor="L,B" Offset="20,10"/>
 	</Grid>
 	
-	<Grid						ID="CQUIOptionsPanel"	Anchor="L,T" AnchorSide="I,O"     Size="220,219" 	Offset="-10,-5" Texture="Tracker_OptionsBacking.dds" SliceCorner="55,61" SliceSize="1,1" SliceTextureSize="121,119" ConsumeAllMouse="1" Hidden="1">
+	<Grid						ID="CQUIOptionsPanel"	Anchor="L,T" AnchorSide="I,O"     Size="220,230" 	Offset="-10,-5" Texture="Tracker_OptionsBacking.dds" SliceCorner="55,61" SliceSize="1,1" SliceTextureSize="121,119" ConsumeAllMouse="1" Hidden="1">
 		<Label															Anchor="C,T" String="CQUI Options" Offset="-6,10" Style="FontFlair16" Color0="106,93,69,255" Color1="0,0,0,150" Color2="146,133,109,255" FontStyle="Glow" SmallCaps="20" SmallCapsLeading="0" SmallCapsType="EveryWord" KerningAdjustment="0"/>
-		<ScrollPanel	ID="CQUIOptionsList"							   Offset="10,30" Size="parent,parent" Style="ScrollPanelWithLeftBar"         HideScrollBar="1" >
-			<Stack			ID="CQUIOptionsStack"	Anchor="R,C"											 StackGrowth="Bottom" Offset="25,-30" StackPadding="1" >
+			<Stack			ID="CQUIOptionsStack"	Anchor="R,C" Size="200,100"											 StackGrowth="Bottom" Offset="25,-30" StackPadding="1" >
+				<Container Size="100,65" Anchor="R,T" />
 				<CheckBox	ID="QUI_ToggleLuxury"			IsChecked="1" 	Offset="10,4"	TextOffset="-5,0" String="QUI: Show Luxury" ToolTip="Shows Luxury Resource in Top Panel" Style="WhiteSemiBold14" ButtonTexture="Controls_Checkbox.dds"	ButtonSize="17,17" CheckTexture="Controls_Checkbox.dds"	CheckSize="17,17" CheckTextureOffset="0,17"  Anchor="R,T"/>
 				<CheckBox	ID="QUI_ToggleSmartBanner"			IsChecked="1" 	Offset="10,4"	TextOffset="-5,0" String="QUI: Smart Banner" ToolTip="Shows more information on city banners" Style="WhiteSemiBold14" ButtonTexture="Controls_Checkbox.dds"	ButtonSize="17,17" CheckTexture="Controls_Checkbox.dds"	CheckSize="17,17" CheckTextureOffset="0,17"  Anchor="R,T"/>
 				<CheckBox	ID="QUI_ToggleCowboy"			IsChecked="0" 	Offset="10,4"	TextOffset="-5,0" String="QUI: Cowboy" ToolTip="Automatically trains horsemen" Style="WhiteSemiBold14" ButtonTexture="Controls_Checkbox.dds"	ButtonSize="0,0" CheckTexture="Controls_Checkbox.dds"	CheckSize="17,17" CheckTextureOffset="0,17"  Anchor="R,T" Hidden="1"/>
 				<CheckBox	ID="QUI_ToggleBadCowboy"			IsChecked="0" 	Offset="10,4"	TextOffset="-5,0" String="QUI: Bad Cowboy" ToolTip="Automatically deletes horsemen" Style="WhiteSemiBold14" ButtonTexture="Controls_Checkbox.dds"	ButtonSize="0,0" CheckTexture="Controls_Checkbox.dds"	CheckSize="17,17" CheckTextureOffset="0,17"  Anchor="R,T" Hidden="1"/>
 				<CheckBox	ID="CQUI_ToggleTechPopup"			IsChecked="0" 	Offset="10,4"	TextOffset="-5,0" String="CQUI: Popup Visual" ToolTip="Tech/Civic popups display when enabled" Style="WhiteSemiBold14" ButtonTexture="Controls_Checkbox.dds"	ButtonSize="17,17" CheckTexture="Controls_Checkbox.dds"	CheckSize="17,17" CheckTextureOffset="0,17"  Anchor="R,T"/>
 				<CheckBox	ID="CQUI_ToggleTechVoiceover"			IsChecked="1" 	Offset="10,4"	TextOffset="-5,0" String="CQUI: Popup Voiceover" ToolTip="Voiceovers from Tech/Civic popups play when enabled, even if the popup visual is disabled" Style="WhiteSemiBold14" ButtonTexture="Controls_Checkbox.dds"	ButtonSize="17,17" CheckTexture="Controls_Checkbox.dds"	CheckSize="17,17" CheckTextureOffset="0,17"  Anchor="R,T"/>
+				<Label Anchor="C,T" String="Binding Options" Offset="-6,10" Style="FontFlair12" Color0="106,93,69,255" Color1="0,0,0,150" Color2="146,133,109,255" FontStyle="Glow" SmallCaps="20" SmallCapsLeading="0" SmallCapsType="EveryWord" KerningAdjustment="0"/>
+				<Stack Anchor="R,T" Offset="20,5" StackGrowth="Bottom" StackPadding="1">
+					<Container Size="200,30" AutoSize="1" Anchor="R,T">
+						<CheckBox	ID="CQUI_ToggleBindings2" Anchor="R,T"	Style="WorldTrackerCheckBox" TextOffset="-5" String="Enhanced" WrapWidth="180" Align="Right"
+							ToolTip="Civ V Binds with the following changes:[NEWLINE]  WASD camera control[NEWLINE]  Q/E unit/city cycling[NEWLINE]  Shift toggles city/unit selection[NEWLINE]  Quarry/Airstrike are moved to alt-key + Q/S[NEWLINE]  NOTE: UNBIND W IN SETTINGS OR WASD WON'T WORK!"
+						/>
+					</Container>
+					<Container Size="200,30" AutoSize="1" Anchor="R,T">
+						<CheckBox	ID="CQUI_ToggleBindings1" IsChecked="1" Anchor="R,T"	Style="WorldTrackerCheckBox" TextOffset="-5" String="Classic" WrapWidth="180" Align="Right"
+							ToolTip="Civ V Binds, Classic!"
+						/>
+					</Container>
+					<Container Size="200,30" AutoSize="1" Anchor="R,T">
+						<CheckBox	ID="CQUI_ToggleBindings0"			Anchor="R,T"	Style="WorldTrackerCheckBox" TextOffset="-5" String="Standard" WrapWidth="180" Align="Right"
+							ToolTip="Civ VI Binds (No Changes)"
+						/>
+					</Container>
+				</Stack>
 			</Stack>
-		</ScrollPanel>
 		<Image Texture="Controls_ButtonExtendSmall" TextureOffset="0,60" Size="20,20" Anchor="L,B" Offset="20,10"/>
 	</Grid>
 

UI/Panels/CityPanel.lua
@@ -1012,6 +1012,8 @@ function Initialize()
 	LuaEvents.ProductionPanel_Close.Add( OnProductionPanelClose );
 	LuaEvents.Tutorial_CityPanelOpen.Add( OnTutorialOpen );
 	LuaEvents.Tutorial_ContextDisableItems.Add( OnTutorial_ContextDisableItems );
+	LuaEvents.CQUI_GoNextCity.Add( CQUI_OnNextCity );
+	LuaEvents.CQUI_GoPrevCity.Add( CQUI_OnPreviousCity );
 
 	-- Truncate possible static text overflows
 	TruncateStringWithTooltip(Controls.BreakdownLabel,	MAX_BEFORE_TRUNC_STATIC_LABELS,	Controls.BreakdownLabel:GetText());

qui.modinfo
@@ -17,6 +17,7 @@
 				<File>UI/MinimapPanel.xml</File>
 				<File>UI/TopPanel.lua</File>
 				<File>UI/UnitPanel.xml</File>
+				<File>UI/WorldInput.lua</File>
 				<File>UI/WorldViewIconsManager.lua</File>
 				<File>UI/Civilopedia/CivilopediaSupport.lua</File>
 				<File>UI/Panels/CityPanel.lua</File>
@@ -69,6 +70,7 @@
 		<File>UI/MinimapPanel.xml</File>
 		<File>UI/TopPanel.lua</File>
 		<File>UI/UnitPanel.xml</File> 
+		<File>UI/WorldInput.lua</File>
 		<File>UI/WorldViewIconsManager.lua</File>
 		<File>UI/Civilopedia/CivilopediaSupport.lua</File>
 		<File>UI/Panels/CityPanel.lua</File>

commit 2cd07136544b15fc7884aec2cbed4251a01be2d5

Fixed issue introduced in 70c5fbbcffa08d36576220abc01813cc7c295d92

UI/WorldInput.lua
@@ -1248,7 +1248,6 @@ end
 function OnInterfaceModeEnter_CityManagement( eNewMode:number )
   UIManager:SetUICursor(CursorTypes.RANGE_ATTACK);  
   UILens.SetActive("CityManagement");
-  UILens.SetLayerHexesColoredArea( LensLayers.CITY_MANAGEMENT, Game.GetLocalPlayer(), UI.GetHeadSelectedCity():GetCulture():GetNextPlot(), UI.GetColorValue("COLOR_DISGUSTING_APPEAL") );
 end
 
 -- ===========================================================================