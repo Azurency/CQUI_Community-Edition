7d1f472c11ce30f93d57aff1833f024c97517580
chaorace <chaorace@gmail.com>
2017-01-19 01:52:13 -0500

    Added CQUI_TopLayer.lua/xml and CQUI_Main.lua
    These are the first elements added using the <UserInterface> and
    <GameplayScripts> tags. Hopefully they'll prove useful and good for
    reducing the complexity of CQUI in the future. I'd like to eventually
    convert the CQUI_SettingsElement into using the <UserInterface> tag and to
    also move the Cityview logic from the CityPanel to the CQUI_Main method

---
+++ Assets/CQUI_Main.lua
@@ -0,0 +1,7 @@
+--This is the main CQUI script. There's not much here yet!
+
+function Initialize()
+  print("CQUI Loaded!");
+end
+
+Initialize();
---
+++ Assets/CQUI_TopLayer.lua
@@ -0,0 +1,7 @@
+--This is a layer that lives at the very top of the UI. It's an excellent place for catching inputs globally or displaying a custom overlay
+
+function Initialize()
+  ContextPtr:SetHide(false);
+end
+
+Initialize();
