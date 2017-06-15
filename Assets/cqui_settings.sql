d10accf430d4c78dcc2f3d080c2a0762238334f1
Chris <chaorace@gmail.com>
2017-06-15 19:35:15 -0400

    Adjusted default settings
    This commit adjusts the default settings to be less confusing for new users
    
    The bindings mode has been changed to "Enhanced" by default, since users keep complaining they lose their WASD binds when using CQUI
    The SmartBanner Unlocked Citizens icon has been disabled by default, since new users were confused by this icon and would frequently complain about it
    The default size of the Smart Work Icons has been lowered, as well as the default base size, to make the icons less offensive to look at without needing to adjust the setting.
    Permanently set the number of deck slots to 9!

--- Assets/cqui_settings.sql
+++ Assets/cqui_settings.sql
@@ -43,7 +43,7 @@ INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
       ("CQUI_ShowUnitPaths", 1), -- Shows unit paths on hover and selection
       ("CQUI_ShowYieldsOnCityHover", 1), -- Shows city management info like citizens, tile yields, and tile growth on hover
       ("CQUI_Smartbanner", 1), -- Additional informations such as districts will show in the city banner
-      ("CQUI_Smartbanner_UnlockedCitizen", 1), -- Shows if city have Unmanaged citizens in the banner
+      ("CQUI_Smartbanner_UnlockedCitizen", 0), -- Shows if city have Unmanaged citizens in the banner
       ("CQUI_Smartbanner_Districts", 1), -- Shows city districts in the banner
       ("CQUI_Smartbanner_Population", 1), -- Shows turns to city population growth in the banner
       ("CQUI_Smartbanner_Cultural", 1), -- Shows turns to city cultural growth in the banner
@@ -63,7 +63,7 @@ INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
 */
 
 INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
-  VALUES  ("CQUI_BindingsMode", 1), -- Set of keybindings used │ 0=Civ6 default │ 1=keybinds from Civ5 │ 2=Civ5 with additions such as WASD camera control |
+  VALUES  ("CQUI_BindingsMode", 2), -- Set of keybindings used │ 0=Civ6 default │ 1=keybinds from Civ5 │ 2=Civ5 with additions such as WASD camera control |
       ("CQUI_ResourceDimmingStyle", 1); -- Affects the way resource icons look when they have been improved  | 0=No Change | 1=Transparent | 2=Hidden |
 
 /*
@@ -79,9 +79,9 @@ INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
 INSERT OR REPLACE INTO CQUI_Settings -- Don't touch this line!
   VALUES  ("CQUI_MinimapSize", 512), -- Factor used for setting minimap size (ex: 512 = 512x256). Recommended values fall between 224 and 768, though any positive could work
   ("CQUI_ProductionItemHeight", 32), -- Height used for individual items in the production queue. Recommended values fall between 24 and 128, though any positive could work
-  ("CQUI_SmartWorkIconSize", 88), -- Size used for "smart" work icons. This size is applied to work icons that are currently locked if the smart work icon option is enabled. Recommended values fall between 48 and 128, though any positive multiple of 8 could work (non-multiples are rounded down)
+  ("CQUI_SmartWorkIconSize", 80), -- Size used for "smart" work icons. This size is applied to work icons that are currently locked if the smart work icon option is enabled. Recommended values fall between 48 and 128, though any positive multiple of 8 could work (non-multiples are rounded down)
   ("CQUI_SmartWorkIconAlpha", 40), -- Transparency percent used for "smart" work icons. This alpha is applied to work icons that are currently locked if the smart work icon option is enabled. Recommended values fall between 10 and 100, though any value between 0 and 100 could work
-  ("CQUI_WorkIconSize", 64), -- Size used for work icons. Applies to all icons that aren't flagged using the "smart" work icon feature. Recommended values fall between 48 and 128, though any positive multiple of 8 could work (non-multiples are rounded down)
+  ("CQUI_WorkIconSize", 56), -- Size used for work icons. Applies to all icons that aren't flagged using the "smart" work icon feature. Recommended values fall between 48 and 128, though any positive multiple of 8 could work (non-multiples are rounded down)
   ("CQUI_WorkIconAlpha", 75); -- Size used for work icons. Applies to all icons that aren't flagged using the "smart" work icon feature. Recommended values fall between 10 and 100, though any value between 0 and 100 could work
 
 /*
