Azurency/CQUI_Community-Edition

commit 4c180a78201e94efef6f42e4eba6633f32220768

Merge Summer 2017 Patch (CQUI\Assets\UI)
Merge Summer 2017 Patch (CQUI\Assets\UI)
open Tasks see #591

Assets/UI/Panels/productionpanel.lua
@@ -246,6 +246,14 @@ function BuildBuilding(city, buildingEntry)
     return;
   end
 
+	-- If it's a Wonder and the city already has the building then it doesn't need to be replaced.
+	if (bNeedsPlacement) then
+		local cityBuildings = city:GetBuildings();
+		if (cityBuildings:HasBuilding(buildingEntry.Hash)) then
+			bNeedsPlacement = false;
+		end
+	end
+
   -- Does the building need to be placed?
   if ( bNeedsPlacement ) then
     --If we were already placing something, quickly pop into selection mode, signalling to CQUI cityview code that placement was interrupted and resetting the view
@@ -315,14 +323,15 @@ function PurchaseUnit(city, unitEntry, purchaseType)
   if (purchaseType == nil) then
     if (unitEntry.Yield == "YIELD_GOLD") then
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_GOLD"].Index;
+		UI.PlaySound("Purchase_With_Gold");
     else
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_FAITH"].Index;
+		UI.PlaySound("Purchase_With_Faith");
     end
   else
     tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = purchaseType;
   end
   CityManager.RequestCommand(city, CityCommandTypes.PURCHASE, tParameters);
-  UI.PlaySound("Purchase_With_Gold");
 end
 
 -- ===========================================================================
@@ -333,14 +342,15 @@ function PurchaseUnitCorps(city, unitEntry, purchaseType)
   if (purchaseType == nil) then
     if (unitEntry.Yield == "YIELD_GOLD") then
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_GOLD"].Index;
+		UI.PlaySound("Purchase_With_Gold");
     else
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_FAITH"].Index;
+		UI.PlaySound("Purchase_With_Faith");
     end
   else
     tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = purchaseType;
   end
   CityManager.RequestCommand(city, CityCommandTypes.PURCHASE, tParameters);
-  UI.PlaySound("Purchase_With_Gold");
 end
 
 -- ===========================================================================
@@ -351,14 +361,15 @@ function PurchaseUnitArmy(city, unitEntry, purchaseType)
   if (purchaseType == nil) then
     if (unitEntry.Yield == "YIELD_GOLD") then
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_GOLD"].Index;
+		UI.PlaySound("Purchase_With_Gold");
     else
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_FAITH"].Index;
+		UI.PlaySound("Purchase_With_Faith");
     end
   else
     tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = purchaseType;
   end
   CityManager.RequestCommand(city, CityCommandTypes.PURCHASE, tParameters);
-  UI.PlaySound("Purchase_With_Gold");
 end
 
 -- ===========================================================================
@@ -368,14 +379,15 @@ function PurchaseBuilding(city, buildingEntry, purchaseType)
   if(purchaseType == nil) then
     if (buildingEntry.Yield == "YIELD_GOLD") then
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_GOLD"].Index;
+		UI.PlaySound("Purchase_With_Gold");
     else
       tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = GameInfo.Yields["YIELD_FAITH"].Index;
+		UI.PlaySound("Purchase_With_Faith");
     end
   else
     tParameters[CityCommandTypes.PARAM_YIELD_TYPE] = purchaseType;
   end
   CityManager.RequestCommand(city, CityCommandTypes.PURCHASE, tParameters);
-  UI.PlaySound("Purchase_With_Gold");
 end
 
 -- ===========================================================================
@@ -2040,7 +2052,7 @@ function Refresh()
         end
 
         local allReasons      :string = ComposeFailureReasonStrings( isDisabled, results );
-        local sToolTip        :string = ToolTipHelper.GetDistrictToolTip( row.Hash ) .. allReasons;
+        local sToolTip        :string = ToolTipHelper..GetToolTip(row.DistrictType, Game.GetLocalPlayer()) .. allReasons;
 
         local iProductionCost   :number = buildQueue:GetDistrictCost( row.Index );
         local iProductionProgress :number = buildQueue:GetDistrictProgress( row.Index );
@@ -2293,7 +2305,7 @@ function Refresh()
           Kind      = row.Kind,
           TurnsLeft   = buildQueue:GetTurnsLeft( row.Hash ),
           Disabled    = isDisabled,
-          Repair      = cityBuildings:IsPillaged( row.Index ),
+          Repair      = cityBuildings:IsPillaged( row.Hash ),
           Cost      = iProductionCost,
           Progress    = iProductionProgress,
           IsWonder    = row.IsWonder,
@@ -2353,7 +2365,8 @@ function Refresh()
           ArmyCost    = 0,
           ArmyTurnsLeft = 1,
           ArmyTooltip   = "",
-          ArmyName    = ""
+          ArmyName    = "",
+          ReligiousStrength	= row.ReligiousStrength
         };
 
         -- Should we present options for building Corps or Army versions?

Assets/UI/Choosers/researchchooser.lua
@@ -256,12 +256,13 @@ end
 -- ===========================================================================
 function RealizeSize()
   local _, screenY:number = UIManager:GetScreenSizeVal();
-  Controls.ChooseResearchList:SetSizeY(screenY - Controls.ChooseResearchList:GetOffsetY() - 30);
-  Controls.ChooseResearchList:CalculateInternalSize();
 
   Controls.ResearchStack:CalculateSize();
   Controls.ResearchStack:ReprocessAnchoring();
 
+	Controls.ChooseResearchList:SetSizeY(screenY - Controls.ChooseResearchList:GetOffsetY() - 30);
+	Controls.ChooseResearchList:CalculateInternalSize();
+	
   if(Controls.ChooseResearchList:GetScrollBar():IsHidden()) then
     Controls.ChooseResearchList:SetOffsetX(10);
   else
@@ -347,7 +348,11 @@ function OnResearchChanged( ePlayer:number, eTech:number )
   if localPlayer ~= -1 and localPlayer == ePlayer then
     local pPlayerTechs :table = Players[localPlayer]:GetTechs();
     m_currentID			= pPlayerTechs:GetResearchingTech();
-    m_lastCompletedID	= -1;
+		
+		-- Only reset last completed tech once a new tech has been selected
+		if m_currentID >= 0 then
+    		m_lastCompletedID	= -1;
+		end
 
     m_needsRefresh = true;
   end

Assets/UI/Civilopedia/civilopediasupport.lua
@@ -41,6 +41,7 @@ _Sections = {};
 _PagesBySection = {};
 _PageGroupsBySection = {};
 _ChaptersByPageLayout = {};
+_ChapterOverrides = {};
 _PageLayoutScriptTemplates = {};
 
 PageLayouts = {};
@@ -185,7 +186,7 @@ function CacheData_FetchData()
     end
   end
 
-  -- Cache Chapters
+	-- Cache Chapters by Page Layout
   if(GameInfo.CivilopediaPageLayoutChapters) then
     for q in GameInfo.CivilopediaPageLayoutChapters() do
       local page_layout = {
@@ -201,12 +202,51 @@ function CacheData_FetchData()
     end
   end
 
+	-- Cache Chapter overrides
+	if(GameInfo.CivilopediaPageChapterHeaders) then
+		for row in GameInfo.CivilopediaPageChapterHeaders() do
+			local key = row.SectionId .. "|" .. row.PageId .. "|" .. row.ChapterId;
+			local chapter = _ChapterOverrides[key] or {};
+			chapter.Header = row.Header;
+			_ChapterOverrides[key] = chapter;			
+		end
+	end
+
+	if(GameInfo.CivilopediaPageChapterParagraphs) then
+		-- Index all paragraphs.
+		local paragraphs = {};
+		for row in GameInfo.CivilopediaPageChapterParagraphs() do
+			local key = row.SectionId .. "|" .. row.PageId .. "|" .. row.ChapterId;
+			local chapter = paragraphs[key] or {};
+			table.insert(chapter, row);
+			paragraphs[key] = chapter;
+		end
+
+		-- Sort the paragraphs, and add to chapter overrides.
+		local sort = function(a,b)	return a.SortIndex < b.SortIndex end;
+
+		for key, v in pairs(paragraphs) do
+			table.sort(v, sort);
+			local chapter = _ChapterOverrides[key] or {};
+			local body = {};
+
+			for i, p in ipairs(v) do
+				table.insert(body, p.Paragraph);
+			end
+
+			chapter.Body = body;
+			_ChapterOverrides[key] = chapter;
+		end
+	end
+	
   -- Cache Layouts
   if(GameInfo.CivilopediaPageLayouts) then
     for q in GameInfo.CivilopediaPageLayouts() do
       _PageLayoutScriptTemplates[q.PageLayoutId] = q.ScriptTemplate;
     end
   end
+
+
 end
 
 
@@ -338,6 +378,7 @@ function CacheData()
   _PagesBySection = {};
   _PageGroupsBySection = {};
   _ChaptersByPageLayout = {};
+	_ChapterOverrides = {};
   _PageLayoutScriptTemplates ={};
 
   CacheData_FetchData();
@@ -355,7 +396,18 @@ function PopulateSearchData()
 
     for sectionId, v in pairs(_PagesBySection) do
       for i, page in ipairs(v) do
-        Search.AddData(searchContext, sectionId .. "|" .. page.PageId, page.Title, "");
+				
+				local pageId = page.PageId;
+				local terms = {};
+
+				for row in GameInfo.CivilopediaPageSearchTerms() do
+					if(row.SectionId == sectionId and row.PageId == pageId) then
+						local term = Locale.Lookup(row.Term);
+						table.insert(terms, term);
+					end
+				end
+				
+				Search.AddData(searchContext, sectionId .. "|" .. pageId, page.Title, "", terms);
       end
     end
 
@@ -424,7 +476,14 @@ end
 -- Will return nil if no text is found.
 -------------------------------------------------------------------------------
 function GetChapterHeader(SectionId, PageId, ChapterId)
-  return FindChapterTextKey(SectionId, PageId, ChapterId, "TITLE");
+	
+	local key = SectionId .. "|" .. PageId .. "|" .. ChapterId;
+	local chapter = _ChapterOverrides[key];
+	if(chapter and chapter.Header) then
+		return chapter.Header;
+	else
+  		return FindChapterTextKey(SectionId, PageId, ChapterId, "TITLE");
+	end
 end
 
 
@@ -433,25 +492,32 @@ end
 -- Returns nil if no text is found.
 -------------------------------------------------------------------------------
 function GetChapterBody(SectionId, PageId, ChapterId)
-  local body_key = FindChapterTextKey(SectionId, PageId, ChapterId, "BODY");
-  if(body_key ~= nil) then
-    return {body_key};
-  end
 
-  local keys = {};
-  local i = 1;
-  repeat
-    key = FindChapterTextKey(SectionId, PageId, ChapterId, "PARA_" .. i);
-    if(key ~= nil) then
-      table.insert(keys, key);
-    end
-    i = i + 1;
-
-  until(key == nil);
-
-  if(#keys > 0) then
-    return keys;
-  end
+	local key = SectionId .. "|" .. PageId .. "|" .. ChapterId;
+	local chapter = _ChapterOverrides[key];
+	if(chapter and chapter.Body and #chapter.Body > 0) then
+		return chapter.Body;
+	else
+	  local body_key = FindChapterTextKey(SectionId, PageId, ChapterId, "BODY");
+	  if(body_key ~= nil) then
+	    return {body_key};
+	  end
+	
+	  local keys = {};
+	  local i = 1;
+	  repeat
+	    key = FindChapterTextKey(SectionId, PageId, ChapterId, "PARA_" .. i);
+	    if(key ~= nil) then
+	      table.insert(keys, key);
+	    end
+	    i = i + 1;
+	
+	  until(key == nil);
+	
+	  if(#keys > 0) then
+	    return keys;
+	  end
+	end
 end
 
 
@@ -734,7 +800,7 @@ function CivilopediaSearch(term, max_results)
 
   -- Neither found.  Time to do full text search!
   if _SearchQuery ~= nil and #_SearchQuery > 0 and _SearchQuery ~= LOC_TREE_SEARCH_W_DOTS then
-    local search_results = Search.Search("Civilopedia", _SearchQuery .. "*");
+		local search_results = Search.Search("Civilopedia", _SearchQuery);
     if (search_results and #search_results > 0) then
       for i, v in ipairs(search_results) do
         local sectionId, pageId = string.match(v[1], "([^|]+)|([^|]+)");
@@ -805,9 +871,10 @@ function OnOpenCivilopedia(sectionId_or_search, pageId)
     print("Searching for " .. sectionId_or_search);
     local results = CivilopediaSearch(sectionId_or_search);
     if(results and #results > 0) then
-      print("Found!");
-      print(results[1].SectionId);
-      print(results[1].PageId);
+		print("Found " .. #results .. " results");
+		for i,v in ipairs(results) do
+			print(v.SectionId .. " - " .. v.PageId);
+		end
       NavigateTo(results[1].SectionId, results[1].PageId);
     else
       -- To the front page!
@@ -869,7 +936,7 @@ function OnSearchCharCallback()
   local has_found = {};
   if str ~= nil and #str > 0 and str ~= LOC_TREE_SEARCH_W_DOTS then
     _SearchQuery = str;
-    local results = Search.Search("Civilopedia", str .. "*");
+		local results = Search.Search("Civilopedia", str);
     _SearchResultsManager:DestroyInstances();
     if (results and #results > 0) then
       for i, v in ipairs(results) do
@@ -878,8 +945,20 @@ function OnSearchCharCallback()
           -- v[2] Page Name
           -- v[3] Page Content (NYI)
           local instance = _SearchResultsManager:GetInstance();
-
           local sectionId, pageId = string.match(v[1], "([^|]+)|([^|]+)");
+					
+			local section;
+			local sections = GetSections();
+			for i,v in ipairs(sections) do
+				if(v.SectionId == sectionId) then
+					section = v;
+					break;
+				end
+			end
+
+			if(section) then
+				instance.Icon:SetIcon(section.Icon);
+			end
 
           -- Search results already localized.
           instance.Name:SetText(v[2]);
@@ -1240,10 +1319,12 @@ function AddPortrait(icon, color)
   end
 end
 
-function AddTallPortrait(image)
-  local instance = _RightColumnTallPortraitManager:GetInstance();
-  -- Todo: Update icon.
-  instance.Root:SetIcon(image);
+function AddTallPortrait(icon)
+	if(icon ~= nil) then
+  		local instance = _RightColumnTallPortraitManager:GetInstance();
+		local success = instance.Root:SetIcon(icon);
+		instance.Root:SetHide(not success);
+	end
   -- Infer two-column layout.
   _PageContentLayout = "two-column";
 end

Assets/UI/Panels/unitpanel.lua
@@ -5,7 +5,7 @@ include( "InstanceManager" );
 include( "SupportFunctions" );
 include( "Colors" );
 include( "CombatInfo" );
-include( "PopupDialogSupport" );
+include( "PopupDialog" );
 include( "Civ6Common" );
 include( "EspionageSupport" );
 
@@ -58,6 +58,7 @@ local m_kSoundCV1               :table = {};
 local m_kTutorialDisabled   :table = {};  -- key = Unit Type, value = lockedHashes
 local m_kTutorialAllDisabled  :table = {};  -- hashes of actions disabled for all units
 
+local m_DeleteInProgress        :boolean = false;
 
 local m_attackerUnit = nil;
 local m_locX = nil;
@@ -461,6 +462,10 @@ function GetUnitActionsTable( pUnit )
 
       local instance;
 
+			if (operationRow.Index == 53) then
+				local foo = bar;
+			end
+
       -- if unit can build an improvement, show all the buildable improvements for that tile
       if (actionHash == UnitOperationTypes.BUILD_IMPROVEMENT) then
         local tParameters = {};
@@ -850,7 +855,9 @@ function View(data)
 
   -- Portrait Icons
   if(data.IconName ~= nil) then
-    Controls.UnitIcon:SetIcon(data.IconName);
+		if not Controls.UnitIcon:SetIcon(data.IconName) then
+			Controls.UnitIcon:SetIcon(data.FallbackIconName)
+		end
   end
   if(data.CivIconName ~= nil) then
     local darkerBackColor = DarkenLightenColor(m_primaryColor,(-85),238);
@@ -992,7 +999,9 @@ function ViewTarget(data)
 
   -- Portrait Icons
   if(data.IconName ~= nil) then
-    Controls.TargetUnitIcon:SetIcon(data.IconName);
+		if not Controls.TargetUnitIcon:SetIcon(data.IconName) then
+			Controls.TargetUnitIcon:SetIcon(data.FallbackIconName)
+		end
   end
   if(data.CivIconName ~= nil) then
     local darkerBackColor = DarkenLightenColor(m_primaryColor,(-85),238);
@@ -1285,6 +1294,7 @@ function AddUnitStat(statType:number, statData:table, unitData:table, relativeSi
   -- Update icon
   local textureOffsetX, textureOffsetY, textureSheet = IconManager:FindIconAtlas(statData.IconName,22);
   statInstance.StatCheckBox:SetCheckTexture(textureSheet);
+	statInstance.StatCheckBox:SetUnCheckTexture(textureSheet)
   statInstance.StatCheckBox:SetCheckTextureOffsetVal(textureOffsetX,textureOffsetY);
   statInstance.StatCheckBox:SetUnCheckTextureOffsetVal(textureOffsetX,textureOffsetY);
 
@@ -1456,14 +1466,6 @@ function OnShowCombat( showCombat )
     pAttacker = UI.GetHeadSelectedUnit();
     if (pAttacker ~= nil) then
       bAttackerIsUnit = true;
-
-      -- Never show combat for civilian units unless it's between two religious untis.
-      if showCombat then
-        if (GameInfo.Units[pAttacker:GetUnitType()].FormationClass == "FORMATION_CLASS_CIVILIAN")
-        and (IsAttackerReligiousCombat(pAttacker)==false) then
-          showCombat = false;   -- All this showCombat logic, should be done before VIEW! :(
-        end
-      end
     else
       return;
     end
@@ -1903,6 +1905,7 @@ function ReadUnitData( unit:table )
   m_subjectData.Name            = unit:GetName();
   m_subjectData.UnitTypeName        = GameInfo.Units[unit:GetUnitType()].Name;
   m_subjectData.IconName          = GetUnitPortraitPrefix(unit:GetOwner())..GameInfo.Units[unit:GetUnitType()].UnitType.."_PORTRAIT";
+	m_subjectData.FallbackIconName			= "ICON_"..GameInfo.Units[unit:GetUnitType()].UnitType.."_PORTRAIT";
   m_subjectData.Moves           = unit:GetMovesRemaining();
   m_subjectData.MovementMoves       = unit:GetMovementMovesRemaining();
   m_subjectData.InFormation       = unit:GetFormationUnitCount() > 1;
@@ -2427,14 +2430,28 @@ end
 -- ===========================================================================
 --  Prompt if the player really wants to force remove a unit.
 -- ===========================================================================
+function OnNoDelete()
+    m_DeleteInProgress = false;
+end
+
 function OnPromptToDeleteUnit()
   local pUnit   :table  = UI.GetHeadSelectedUnit();
+
+    if m_DeleteInProgress then
+        return;
+    end
+
   if(pUnit) then
+		-- Only show the prompt if it's possible to delete this unit
+		local bCanStart = UnitManager.CanStartCommand( pUnit, UnitCommandTypes.DELETE, true);
+		if bCanStart then
     local unitName  :string = GameInfo.Units[pUnit:GetUnitType()].Name;
     local msg   :string = Locale.Lookup("LOC_HUD_UNIT_PANEL_ARE_YOU_SURE_DELETE", unitName);
     -- Pass the unit ID through, the user can take their time in the dialog and it is possible that the selected unit will change
     local unitID = pUnit:GetComponentID();
-    OnDeleteUnit(unitID);
+			m_kPopupDialog:ShowOkCancelDialog( msg, function() OnDeleteUnit(unitID) end, function() OnNoDelete() end);
+            m_DeleteInProgress = true;
+		end
   end
 end
 
@@ -2443,6 +2460,7 @@ end
 --  Resets lens to turn off any that are for the unit (e.g., settler)
 -- ===========================================================================
 function OnDeleteUnit(unitID : table)
+    m_DeleteInProgress = false;
 
   local pUnit   :table  = UnitManager.GetUnit(unitID.player, unitID.id);
   if (pUnit ~= nil) then
@@ -2742,8 +2760,10 @@ function ShowCombatAssessment( )
       end
     end
     if (attacker[CombatResultParameters.FINAL_DAMAGE_TO] + (extraDamage/2) < attacker[CombatResultParameters.MAX_HIT_POINTS]) then
+			if (iDefenderStrength > 0) then
       isSafe = true;
     end
+		end
 
     local combatAssessmentStr :string = "";
     local damageToDefender = defender[CombatResultParameters.DAMAGE_TO];
@@ -2790,7 +2810,7 @@ function ShowCombatAssessment( )
             end
           end
         else
-          if (defender[CombatResultParameters.FINAL_DAMAGE_TO] >= defenderHitpoints) then
+					if (damageToDefender >= defenderHitpoints) then
             combatAssessmentStr = Locale.Lookup("LOC_HUD_UNIT_PANEL_OUTCOME_DECISIVE_VICTORY");
             ShowCombatVictoryBanner();
           else
@@ -2804,7 +2824,7 @@ function ShowCombatAssessment( )
           end
         end
       else  --non ranged attacks
-        if (defender[CombatResultParameters.FINAL_DAMAGE_TO] >= defenderHitpoints and isSafe) then
+				if (damageToDefender >= defenderHitpoints) then
           combatAssessmentStr = Locale.Lookup("LOC_HUD_UNIT_PANEL_OUTCOME_DECISIVE_VICTORY");
           ShowCombatVictoryBanner();
         else
@@ -2872,16 +2892,28 @@ function ShowCombatAssessment( )
         end
       end
     else
+			if (iDefenderStrength > 0) then
       combatAssessmentStr = Locale.Lookup("LOC_HUD_UNIT_PANEL_OUTCOME_INEFFECTIVE");
       ShowCombatStalemateBanner();
+			else
+				combatAssessmentStr = Locale.Lookup("LOC_HUD_UNIT_PANEL_OUTCOME_DECISIVE_VICTORY");
+				ShowCombatVictoryBanner();
+			end
     end
 
     Controls.CombatAssessmentText:SetText(Locale.ToUpper(combatAssessmentStr));
 
     -- Show interceptor information
-    if m_targetData.InterceptorName ~= "" then
+		local interceptorData = m_combatResults[CombatResultParameters.INTERCEPTOR];
+		local interceptorID = interceptorData[CombatResultParameters.ID];
+		local defenderID = defender[CombatResultParameters.ID];
+		local pkInterceptor = UnitManager.GetUnit(interceptorID.player, interceptorID.id);
+		if (pkInterceptor ~= nil and interceptorID.id ~= defenderID.id) then
+			local interceptorStrength = interceptorData[CombatResultParameters.COMBAT_STRENGTH];
+			local interceptorStrengthModifier = interceptorData[CombatResultParameters.STRENGTH_MODIFIER];
+			local modifiedStrength = interceptorStrength + interceptorStrengthModifier;
       Controls.InterceptorName:SetText(m_targetData.InterceptorName);
-      Controls.InterceptorStrength:SetText(m_targetData.InterceptorCombat);
+			Controls.InterceptorStrength:SetText(modifiedStrength);
 
       UpdateInterceptorModifiers(0);
 
@@ -2895,10 +2927,16 @@ function ShowCombatAssessment( )
       Controls.InterceptorGrid:SetHide(true);
     end
 
-    -- Show anit-air information
-    if m_targetData.AntiAirName ~= "" then
+		-- Show anti-air information if the anti-air unit is not the same as the defender
+		local antiAirData = m_combatResults[CombatResultParameters.ANTI_AIR];
+		local antiAirID = antiAirData[CombatResultParameters.ID];
+		local pkAntiAir = UnitManager.GetUnit(antiAirID.player, antiAirID.id);
+		if (pkAntiAir ~= nil and antiAirID.id ~= defenderID.id) then
+			local antiAirStrength = m_combatResults[CombatResultParameters.ANTI_AIR][CombatResultParameters.COMBAT_STRENGTH];
+			local antiAirStrengthModifier = m_combatResults[CombatResultParameters.ANTI_AIR][CombatResultParameters.STRENGTH_MODIFIER];
       Controls.AAName:SetText(m_targetData.AntiAirName);
-      Controls.AAStrength:SetText(m_targetData.AntiAirCombat);
+			local modifiedStrength = antiAirStrength + antiAirStrengthModifier;
+			Controls.AAStrength:SetText(modifiedStrength);
 
       UpdateAntiAirModifiers(0);
 
@@ -3014,7 +3052,7 @@ function OnUnitFlagPointerEntered( playerID:number, unitID:number )
           local pDefender = UnitManager.GetUnit(playerID, unitID);
           if (pDefender ~= nil) then
             m_combatResults = CombatManager.SimulateAttackVersus( pDistrict:GetComponentID(), pDefender:GetComponentID() );
-            isValidToShow = ReadTargetData();
+						isValidToShow = ReadTargetData(attackingCity);		
           end
         end
       end
@@ -3024,7 +3062,7 @@ function OnUnitFlagPointerEntered( playerID:number, unitID:number )
         local pDefender = UnitManager.GetUnit(playerID, unitID);
         if (pDefender ~= nil) then
           m_combatResults = CombatManager.SimulateAttackVersus( pDistrict:GetComponentID(), pDefender:GetComponentID() );
-          isValidToShow = ReadTargetData();
+					isValidToShow = ReadTargetData(pDistrict);		
         end
       end
     else
@@ -3086,7 +3124,7 @@ function InspectPlot( plot:table )
       return false;
     end
     GetCombatResults( pDistrict:GetComponentID(), plot:GetX(), plot:GetY() )
-    isValidToShow = ReadTargetData();
+		isValidToShow = ReadTargetData(pDistrict);
 
   elseif (UI.GetInterfaceMode() == InterfaceModeTypes.DISTRICT_RANGE_ATTACK) then
     local pDistrict = UI.GetHeadSelectedDistrict();
@@ -3094,7 +3132,7 @@ function InspectPlot( plot:table )
       return false;
     end
     GetCombatResults( pDistrict:GetComponentID(), plot:GetX(), plot:GetY() )
-    isValidToShow = ReadTargetData();
+		isValidToShow = ReadTargetData(pDistrict);
 
   else
     local pUnit = UI.GetHeadSelectedUnit();
@@ -3129,18 +3167,6 @@ function IsTargetReligiousCombat(targetData)
   return false;
 end
 
-function IsAttackerCombat(attacker)
-  if
-  (
-    (attacker:GetCombat() > 0)
-  or  (attacker:GetRangedCombat() > 0)
-  or  (attacker:GetBombardCombat() > 0)
-  ) then
-    return true;
-  end
-  return false;
-end
-
 function IsAttackerReligiousCombat(attacker)
   if ( attacker:GetReligiousStrength() > 0 ) then
     return true;
@@ -3168,6 +3194,7 @@ function ReadTargetData(attacker)
         local unitGreatPerson = pkDefender:GetGreatPerson();
         m_targetData.Name           = Locale.Lookup( pkDefender:GetName() );
         m_targetData.IconName         = GetUnitPortraitPrefix( pkDefender:GetOwner() )..GameInfo.Units[pkDefender:GetUnitType()].UnitType.."_PORTRAIT";
+				m_targetData.FallbackIconName			= "ICON_"..GameInfo.Units[pkDefender:GetUnitType()].UnitType.."_PORTRAIT";
         m_targetData.Combat           = pkDefender:GetCombat();
         m_targetData.RangedCombat       = pkDefender:GetRangedCombat();
         m_targetData.BombardCombat        = pkDefender:GetBombardCombat();
@@ -3188,17 +3215,10 @@ function ReadTargetData(attacker)
 
         -- Only display target data if the combat type of the attacker and target match
         if (attacker ~= nil) then
-          if
-          (
-            (IsAttackerCombat(attacker) and IsTargetCombat(m_targetData))
-          or  (IsAttackerReligiousCombat(attacker) and IsTargetReligiousCombat(m_targetData))
-          ) then
-            bShowTarget = true;
-          else
-            bShowTarget = false;
+					local eCombatType = m_combatResults[CombatResultParameters.COMBAT_TYPE];
+					if (eCombatType ~= nil) then
+						bShowTarget = CombatManager.CanAttackTarget(attacker:GetComponentID(), pkDefender:GetComponentID(), eCombatType);
           end
-        else
-          bShowTarget = true;
         end
       end
     elseif (defenderID.type == ComponentType.DISTRICT) then
@@ -3260,12 +3280,15 @@ function ReadTargetData(attacker)
       m_targetData.InterceptorPotentialDamage = m_combatResults[CombatResultParameters.INTERCEPTOR][CombatResultParameters.DAMAGE_TO];
     end
 
-    local antiAirID = m_combatResults[CombatResultParameters.ANTI_AIR][CombatResultParameters.ID];
-    local pkAntiAir = UnitManager.GetUnit(antiAirID.player, antiAirID.id);
-    if (pkAntiAir ~= nil) then
-      m_targetData.AntiAirName      = Locale.Lookup(pkAntiAir:GetName());
-      m_targetData.AntiAirCombat      = pkAntiAir:GetAntiAirCombat();
-    end
+		local antiAirCombatResults = m_combatResults[CombatResultParameters.ANTI_AIR];
+		if (antiAirCombatResults ~= nil) then
+			local antiAirID = antiAirCombatResults [CombatResultParameters.ID];
+		    local pkAntiAir = UnitManager.GetUnit(antiAirID.player, antiAirID.id);
+		    if (pkAntiAir ~= nil) then
+		      m_targetData.AntiAirName      = Locale.Lookup(pkAntiAir:GetName());
+		      m_targetData.AntiAirCombat      = pkAntiAir:GetAntiAirCombat();
+		    end
+		end
 
     if (bShowTarget) then
       ViewTarget(m_targetData);
@@ -3523,7 +3546,7 @@ function AddUnitToUnitList(pUnit:table)
   unitEntry.Button:SetVoids(i, pUnit:GetID());
 
   -- Update unit icon
-  local iconInfo:table, iconShadowInfo:table = GetUnitIconAndIconShadow(pUnit, 22, true);
+	local iconInfo:table, iconShadowInfo:table = GetUnitIcon(pUnit, 22, true);
   if iconInfo.textureSheet then
     unitEntry.UnitTypeIcon:SetTexture( iconInfo.textureOffsetX, iconInfo.textureOffsetY, iconInfo.textureSheet );
   end
@@ -3720,7 +3743,7 @@ function Initialize()
   LuaEvents.PlayerChange_Close.Add(         OnPlayerChangeClose );
 
   -- Popup setup
-  m_kPopupDialog = PopupDialog:new( "UnitPanelPopup" );
+	m_kPopupDialog = PopupDialogInGame:new( "UnitPanelPopup" );
 
   -- Setup settlement water guide colors
   local FreshWaterColor:number = UI.GetColorValue("COLOR_BREATHTAKING_APPEAL");

commit d5698bece925cc019a618e2fd6f08a31e2958865

Fixed typo introduced in 4c180a78

Assets/UI/Panels/productionpanel.lua
@@ -2052,7 +2052,7 @@ function Refresh()
         end
 
         local allReasons      :string = ComposeFailureReasonStrings( isDisabled, results );
-        local sToolTip        :string = ToolTipHelper..GetToolTip(row.DistrictType, Game.GetLocalPlayer()) .. allReasons;
+        local sToolTip        :string = ToolTipHelper.GetToolTip(row.DistrictType, Game.GetLocalPlayer()) .. allReasons;
 
         local iProductionCost   :number = buildQueue:GetDistrictCost( row.Index );
         local iProductionProgress :number = buildQueue:GetDistrictProgress( row.Index );