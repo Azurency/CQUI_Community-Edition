abc6f83258d2dc78af79cef853dfe65b419cfa07
Lassier Antoine <toinousp@gmail.com>
2018-02-26 22:30:08 +0100

    Added cqui version for debugging

--- Assets/cqui_main.lua
+++ Assets/cqui_main.lua
@@ -1,7 +1,7 @@
 --This is the main CQUI script. There's not much here yet!
 
 function Initialize()
-  print("CQUI Loaded!");
+  print("CQUI Loaded! Update for v1.0.0.216 (beta)");
 end
 
 Initialize();
