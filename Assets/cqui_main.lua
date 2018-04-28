462d524d6ba0190c23b11f5bbc4bb66b25643700
Lassier Antoine <toinousp@gmail.com>
2018-04-28 21:48:40 +0200

    Bumped the logged version date for debug purposes

--- Assets/cqui_main.lua
+++ Assets/cqui_main.lua
@@ -1,7 +1,7 @@
 --This is the main CQUI script. There's not much here yet!
 
 function Initialize()
-  print("CQUI Loaded! Update for v1.0.0.229 (2018-03-09)");
+  print("CQUI Loaded! Update for v1.0.0.229 (2018-04-28)");
 end
 
 Initialize();
