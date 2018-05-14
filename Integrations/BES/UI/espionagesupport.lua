8ccf4a3a62f3049051dffbfdfb31b1ed2ba8b646
megalobrainiac <george@megalobrainiac.com>
2018-05-14 06:24:29 -0400

    Update BES espionage files with changes from Spring Update Patch 1.0.0.257 (#335)

--- Integrations/BES/UI/espionagesupport.lua
+++ Integrations/BES/UI/espionagesupport.lua
@@ -4,7 +4,7 @@ function GetFormattedOperationDetailText(operation:table, spy:table, city:table)
   local eOperation:number = GameInfo.UnitOperations[operation.Hash].Index;
   local sOperationDetails:string = UnitManager.GetOperationDetailText(eOperation, spy, Map.GetPlot(city:GetX(), city:GetY()));
   if operation.OperationType == "UNITOPERATION_SPY_GREAT_WORK_HEIST" then
-    outputString = Locale.Lookup("LOC_SPYMISSIONDETAILS_UNITOPERATION_SPY_GREAT_WORK_HEIST", Locale.Lookup(sOperationDetails));
+    outputString = Locale.Lookup("LOC_SPYMISSIONDETAILS_UNITOPERATION_SPY_GREAT_WORK_HEIST", sOperationDetails);
   elseif operation.OperationType == "UNITOPERATION_SPY_SIPHON_FUNDS" then
     outputString = Locale.Lookup("LOC_SPYMISSIONDETAILS_UNITOPERATION_SPY_SIPHON_FUNDS", Locale.ToUpper(city:GetName()), sOperationDetails);
   elseif sOperationDetails ~= "" then
@@ -35,71 +35,72 @@ function GetSpyRankNameByLevel(level:number)
 end
 
 -- ===========================================================================
+function GetMissionDescriptionString(mission:table, noloot:string, withloot:string)
+  if mission.LootInfo >= 0 then
+    return Locale.Lookup(withloot, GetMissionLootString(mission), mission.CityName);
+  end
+
+  return Locale.Lookup(noloot, mission.CityName);
+end
+
+-- ===========================================================================
 function GetMissionOutcomeDetails(mission:table)
   local outcomeDetails:table = {};
   if mission.InitialResult == EspionageResultTypes.SUCCESS_UNDETECTED then
     -- Success and undetected
-    if mission.LootInfo >= 0 then
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_UNDETECTED_STOLELOOT", GetMissionLootString(mission), Locale.Lookup(mission.CityName));
-    else
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_UNDETECTED", Locale.Lookup(mission.CityName));
-    end
     outcomeDetails.Success = true;
+    outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_UNDETECTED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_UNDETECTED_STOLELOOT");
     outcomeDetails.SpyStatus = "";
   elseif mission.InitialResult == EspionageResultTypes.SUCCESS_MUST_ESCAPE then
     -- Success but detected
     if mission.EscapeResult == EspionageResultTypes.FAIL_MUST_ESCAPE then
       -- Success and escaped
-      if mission.LootInfo >= 0 then
-        outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_STOLELOOT", GetMissionLootString(mission), Locale.Lookup(mission.CityName));
-      else
-        outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_ESCAPED", Locale.Lookup(mission.CityName));
-      end
       outcomeDetails.Success = true;
+      outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_ESCAPED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_STOLELOOT");
       outcomeDetails.SpyStatus = "";
     elseif mission.EscapeResult == EspionageResultTypes.KILLED then
       -- Success and killed
       outcomeDetails.Success = false;
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_KILLED", Locale.Lookup(mission.CityName));
+      outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_KILLED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_KILLED_STOLELOOT");
       outcomeDetails.SpyStatus = Locale.ToUpper("LOC_ESPIONAGEOVERVIEW_SPYKILLED");
     elseif mission.EscapeResult == EspionageResultTypes.CAPTURED then
       -- Success and captured
       outcomeDetails.Success = false;
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_CAPTURED", Locale.Lookup(mission.CityName));
+      outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_CAPTURED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_SUCCESS_DETECTED_CAPTURED_STOLELOOT");
       outcomeDetails.SpyStatus = Locale.ToUpper("LOC_ESPIONAGEOVERVIEW_SPYCAUGHT");
     end
   elseif mission.InitialResult == EspionageResultTypes.FAIL_UNDETECTED then
     -- Failure but undetected
     outcomeDetails.Success = false;
-    outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_UNDETECTED", Locale.Lookup(mission.CityName));
+    outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_UNDETECTED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_UNDETECTED_STOLELOOT");
     outcomeDetails.SpyStatus = "";
   elseif mission.InitialResult == EspionageResultTypes.FAIL_MUST_ESCAPE then
     -- Failure and detected
     if mission.EscapeResult == EspionageResultTypes.FAIL_MUST_ESCAPE then
       -- Failure and escaped
       outcomeDetails.Success = false;
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_ESCAPED", Locale.Lookup(mission.CityName));
+      outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_ESCAPED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_ESCAPED_STOLELOOT");
       outcomeDetails.SpyStatus = "";
     elseif mission.EscapeResult == EspionageResultTypes.KILLED then
       -- Failure and killed
       outcomeDetails.Success = false;
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_KILLED", Locale.Lookup(mission.CityName));
+      outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_KILLED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_KILLED_STOLELOOT");
       outcomeDetails.SpyStatus = Locale.ToUpper("LOC_ESPIONAGEOVERVIEW_SPYKILLED");
     elseif mission.EscapeResult == EspionageResultTypes.CAPTURED then
       -- Failure and captured
       outcomeDetails.Success = false;
-      outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_CAPTURED", Locale.Lookup(mission.CityName));
+      outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_CAPTURED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_FAILURE_DETECTED_CAPTURED_STOLELOOT");
       outcomeDetails.SpyStatus = Locale.ToUpper("LOC_ESPIONAGEOVERVIEW_SPYCAUGHT");
     end
   elseif mission.InitialResult == EspionageResultTypes.KILLED then
     -- Killed
     outcomeDetails.Success = false;
-    outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_KILLED", Locale.Lookup(mission.CityName));
+    outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_KILLED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_KILLED_STOLELOOT");
     outcomeDetails.SpyStatus = Locale.ToUpper("LOC_ESPIONAGEOVERVIEW_SPYKILLED");
   elseif mission.InitialResult == EspionageResultTypes.CAPTURED then
     -- Captured
     outcomeDetails.Success = false;
-    outcomeDetails.Description = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_CAPTURED", Locale.Lookup(mission.CityName));
+    outcomeDetails.Description = GetMissionDescriptionString(mission, "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_CAPTURED", "LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_CAPTURED_STOLELOOT");
     outcomeDetails.SpyStatus = Locale.ToUpper("LOC_ESPIONAGEOVERVIEW_SPYCAUGHT");
   end
 
@@ -113,13 +114,17 @@ function GetMissionLootString(mission:table)
   local operationInfo:table = GameInfo.UnitOperations[mission.Operation];
   if operationInfo.Hash == UnitOperationTypes.SPY_STEAL_TECH_BOOST then
     local techInfo:table = GameInfo.Technologies[mission.LootInfo];
-    lootString = Locale.Lookup(techInfo.Name);
+    lootString = techInfo.Name;
   elseif operationInfo.Hash == UnitOperationTypes.SPY_GREAT_WORK_HEIST then
     local greatWorkType:number = Game.GetGreatWorkTypeFromIndex(mission.LootInfo);
     local greatWorkInfo:table = GameInfo.GreatWorks[greatWorkType];
-    lootString = Locale.Lookup(greatWorkInfo.Name);
+    lootString = greatWorkInfo.Name;
   elseif operationInfo.Hash == UnitOperationTypes.SPY_SIPHON_FUNDS then
-    lootString = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_GOLD", mission.LootInfo);
+    if mission.LootInfo == 0 then
+      lootString = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_NO_GOLD");
+    else
+      lootString = Locale.Lookup("LOC_ESPIONAGEOVERVIEW_MISSIONOUTCOME_GOLD", mission.LootInfo);
+    end
   end
 
   return lootString;
