e682907a9c09ac5aee56ee756e1effe3b4bcf7b4
Lassier Antoine <toinousp@gmail.com>
2018-07-27 20:30:18 +0200

    Fixes agreement type items in IDS

--- Assets/cqui_main.lua
+++ Assets/cqui_main.lua
@@ -1,7 +1,7 @@
 --This is the main CQUI script. There's not much here yet!
 
 function Initialize()
-  print("CQUI Loaded! Update for v1.0.0.229 (2018-03-09)");
+  print("CQUI Loaded! Update for v1.0.0.262 (2018-07-27)");
 end
 
 Initialize();
